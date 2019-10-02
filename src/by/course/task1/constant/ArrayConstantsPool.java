75201d5b50aec246f47c00b2efe0e3cc9b5b7594
Pobol319 <pobol319@gmail.com>
2019-10-02 20:10:52 +0300

    Работа в классе 30.09.2019

--- src/by/course/task1/constant/ArrayConstantsPool.java
+++ src/by/course/task1/constant/ArrayConstantsPool.java
@@ -5,4 +5,8 @@ public class ArrayConstantsPool {
     public static final int DEFAULT_ARRAY_SIZE = 10;
     public static int DEFAULT_RANDOM_VALUE_BOUND = 100;
 
+    private ArrayConstantsPool() {
+
+    }
+
 }
